b45cabcd1ecb5dc8e89d680195bf2c429bf5e8ae
naji-moha <162563176+naji-moha@users.noreply.github.com>
2025-03-15 15:26:59 +0700

    vite.config.js

--- my-todo-app/vite.config.js
+++ my-todo-app/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/WAD_Week5_assigment/", // Use your actual repository name
   plugins: [react()],
 });
